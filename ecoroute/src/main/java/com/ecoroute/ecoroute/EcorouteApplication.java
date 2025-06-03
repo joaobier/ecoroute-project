1968b9c5d6201327d2439a9fbed57850fd7ac048
joaobier <168764876+joaobier@users.noreply.github.com>
2025-06-03 02:04:59 -0300

    primeiro commit

---
+++ ecoroute/src/main/java/com/ecoroute/ecoroute/EcorouteApplication.java
@@ -0,0 +1,13 @@
+package com.ecoroute.ecoroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcorouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcorouteApplication.class, args);
+	}
+
+}
